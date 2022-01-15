5a7866c1e6111adf37ee6fc5194a81412c8fc293
Graumenth <tar_helcaraxe@yahoo.com>
2022-01-15 17:42:28 +0300

    Blog view + migrations

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\AdminsController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -13,3 +14,4 @@ Auth::routes();
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('/admin', [AdminsController::class, 'index'])->name('admin.index');
+Route::get('/post', [PostController::class, 'show'])->name('post');
