406b34220c75664f0a5bd9721fddb4a40219b63e
Graumenth <tar_helcaraxe@yahoo.com>
2022-01-18 17:28:23 +0300

    Accessor and Mutator examples. Problems with docker etc. docker exec -it cms /bin/sh ||||| su sail ||||| php artisan storage:link solved the problem

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,15 @@ class Post extends Model
     public function user(){
         return $this->belongsTo(User::class);
     }
+
+//    public function setPostImageAttribute($value){
+//        $this->attributes['post_image'] = asset($value);
+//    }
+
+    public function getPostImageAttribute($value){
+        if (strpos($value, 'https://') !== FALSE || strpos($value, 'http://') !== FALSE) {
+            return $value;
+        }
+        return asset('storage/' . $value);
+    }
 }
